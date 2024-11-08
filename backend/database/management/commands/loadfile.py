02fcfc0c2cc935098cd964cf8e17e100af4b2f03
Wei Han <chuaweihan0101@gmail.com>
2024-11-08 12:03:26 -0800

    Add custom command to load data file
    - Run `python manage.py loadfile` to load a local csv datafile into database
    - Currently only handles the specific file in `testdata` directory

---
+++ backend/database/management/commands/loadfile.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from django.core.management import BaseCommand
+from database.models import CellLine, Feature, CellLineFeatureValue
+
+class Command(BaseCommand):
+    help = "Reads in a CSV file and stores data to database"
+
+    def handle(self, *args, **kwargs):
+        filepath = "testdata/cell-lines-nuclear-features.csv"
+
+        try:
+            df = pd.read_csv(filepath)
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f"Error reading file: {e}"))
+            return
+
+        total_rows = len(df)
+        self.stdout.write(self.style.SUCCESS(f"Successfully loaded {filepath}. {total_rows} rows received."))
+
+        for i, row in df.iterrows():
+            self.stdout.write(self.style.SUCCESS(f"Processing row {i} of {total_rows}"))
+
+            # Extract cell line name and depmap id
+            # Note: most sheets will not need to process the slide name
+            slide_name = row.iloc[0]
+            cellline_name = slide_name[:slide_name.find("_")]
+            depmap_id = slide_name[slide_name.find("_") + 1 : slide_name.find(".")]
+            features = row.iloc[1:]
+
+            # Add to CellLine model
+            cellline_obj, created = CellLine.objects.get_or_create(depmap_id=depmap_id, name=cellline_name)
+
+            for feature_name, value in features.items():
+                feature_obj, created = Feature.objects.get_or_create(name=feature_name)
+                CellLineFeatureValue.objects.update_or_create(cellline=cellline_obj, feature=feature_obj, value=value)
