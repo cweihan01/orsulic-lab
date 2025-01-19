67ae0c4c156c3fd42599e5f2cd2d4da5ab5741dd
Wei Han <chuaweihan0101@gmail.com>
2025-01-19 00:18:11 -0800

    Migrate page layout and query form to tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,37 +4,41 @@ import QueryForm from './components/QueryForm';
 import CorrelationResult from './components/CorrelationResult';
 import { mockCorrelationData } from './components/MockData';
 import './App.css'; // Importing App.css for styling
+import './index.js';
 
 function App() {
     const [data, setData] = useState([]);
 
     const handleQuery = (query) => {
-        const filteredData = mockCorrelationData.filter((item) => {
-            const meetsFeature1 = query.feature1
-                ? item.feature_1_name.toLowerCase().includes(query.feature1.toLowerCase())
-                : true;
-            const meetsFeature2 = query.feature2
-                ? item.feature_2_name.toLowerCase().includes(query.feature2.toLowerCase())
-                : true;
-            const meetsCorrelation = query.minCorrelation
-                ? item.spearman_correlation >= parseFloat(query.minCorrelation)
-                : true;
-            const meetsPValue = query.maxPValue
-                ? item.spearman_p_value <= parseFloat(query.maxPValue)
-                : true;
+        console.log("Handing query...")
+        // const filteredData = mockCorrelationData.filter((item) => {
+        //     const meetsFeature1 = query.feature1
+        //         ? item.feature_1_name.toLowerCase().includes(query.feature1.toLowerCase())
+        //         : true;
+        //     const meetsFeature2 = query.feature2
+        //         ? item.feature_2_name.toLowerCase().includes(query.feature2.toLowerCase())
+        //         : true;
+        //     const meetsCorrelation = query.minCorrelation
+        //         ? item.spearman_correlation >= parseFloat(query.minCorrelation)
+        //         : true;
+        //     const meetsPValue = query.maxPValue
+        //         ? item.spearman_p_value <= parseFloat(query.maxPValue)
+        //         : true;
 
-            return meetsFeature1 && meetsFeature2 && meetsCorrelation && meetsPValue;
-        });
+        //     return meetsFeature1 && meetsFeature2 && meetsCorrelation && meetsPValue;
+        // });
 
-        // Sort results by Spearman P-Value in ascending order
-        const sortedData = filteredData.sort((a, b) => a.spearman_p_value - b.spearman_p_value);
+        // // Sort results by Spearman P-Value in ascending order
+        // const sortedData = filteredData.sort((a, b) => a.spearman_p_value - b.spearman_p_value);
 
-        setData(sortedData);
+        // setData(sortedData);
     };
 
     return (
-        <div className="App">
-            <h1 className="App-header">Database Query Interface</h1>
+        <div className="text-center content-center">
+            <h1 className="bg-indigo-300 text-4xl text-bold py-3">
+                Database Query Interface
+            </h1>
             <QueryForm onSubmit={handleQuery} />
             <CorrelationResult data={data} />
         </div>
--- frontend/src/components/QueryForm.jsx
+++ frontend/src/components/QueryForm.jsx
@@ -1,9 +1,132 @@
 // QueryForm.js
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+const TEST_FEATURE_LIST = ['A1BG', '5thPercentile', 'A23F', '99thPercentile'];
+
+function MultiSelectDropdown({
+    formFieldName,
+    options,
+    onChange,
+    prompt = 'Select one or more options',
+}) {
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const optionsListRef = useRef(null);
+
+    const handleChange = (e) => {
+        const isChecked = e.target.checked;
+        const option = e.target.value;
+
+        const selectedOptionSet = new Set(selectedOptions);
+
+        if (isChecked) {
+            selectedOptionSet.add(option);
+        } else {
+            selectedOptionSet.delete(option);
+        }
+
+        const newSelectedOptions = Array.from(selectedOptionSet);
+
+        setSelectedOptions(newSelectedOptions);
+        onChange(newSelectedOptions);
+    };
+
+    const isSelectAllEnabled = selectedOptions.length < options.length;
+
+    const handleSelectAllClick = (e) => {
+        e.preventDefault();
+
+        const optionsInputs = optionsListRef.current.querySelectorAll('input');
+        optionsInputs.forEach((input) => {
+            input.checked = true;
+        });
+
+        setSelectedOptions([...options]);
+        onChange([...options]);
+    };
+
+    const isClearSelectionEnabled = selectedOptions.length > 0;
+
+    const handleClearSelectionClick = (e) => {
+        e.preventDefault();
+
+        const optionsInputs = optionsListRef.current.querySelectorAll('input');
+        optionsInputs.forEach((input) => {
+            input.checked = false;
+        });
+
+        setSelectedOptions([]);
+        onChange([]);
+    };
+
+    return (
+        <label className="relative w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 text-gray-700">
+            <input type="checkbox" className="hidden peer" />
+
+            {/* <div className="after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform text-left pl-5"> */}
+            <div className="flex justify-between items-center pl-5">
+                <div className="text-left">
+                    {selectedOptions.length > 0 ? (
+                        <span>
+                            <span className="ml-1 text-blue-500">
+                                {`(${selectedOptions.length} selected)`} {''}
+                            </span>
+                            {selectedOptions[0] + ', ' + (selectedOptions[1] || '') + '...'}
+                        </span>
+                    ) : (
+                        prompt
+                    )}
+                </div>
+                <span className="after:content-['▼'] after:text-xs after:inline-flex peer-checked:after:-rotate-180 after:transition-transform after:ml-2 text-gray-500"></span>
+            </div>
+
+            <div className="absolute bg-gray-100 border rounded-lg transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 overflow-auto">
+                <ul>
+                    <li>
+                        <button
+                            onClick={handleSelectAllClick}
+                            disabled={!isSelectAllEnabled}
+                            className="w-full text-left px-2 py-1 text-blue-600 disabled:opacity-50"
+                        >
+                            {'Select All'}
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleClearSelectionClick}
+                            disabled={!isClearSelectionEnabled}
+                            className="w-full text-left px-2 py-1 text-blue-600 disabled:opacity-50"
+                        >
+                            {'Clear selection'}
+                        </button>
+                    </li>
+                </ul>
+
+                <ul ref={optionsListRef}>
+                    {options.map((option, i) => {
+                        return (
+                            <li key={option}>
+                                <label className="flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200">
+                                    <input
+                                        type="checkbox"
+                                        name={formFieldName}
+                                        value={option}
+                                        className="cursor-pointer"
+                                        onChange={handleChange}
+                                    />
+                                    <span className="ml-1">{option}</span>
+                                </label>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </label>
+    );
+}
 
 function QueryForm({ onSubmit }) {
     const [feature1, setFeature1] = useState('');
-    const [feature2, setFeature2] = useState('');
+    const [feature2, setFeature2] = useState([]);
     const [minCorrelation, setMinCorrelation] = useState('');
     const [maxPValue, setMaxPValue] = useState('');
 
@@ -15,43 +138,107 @@ function QueryForm({ onSubmit }) {
     };
 
     return (
-        <div className="query-form">
-            <h2>Query Form</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Feature 1:</label>
-                    <input
-                        type="text"
+        <div className="max-w-4xl mx-auto rounded-lg drop-shadow-lg bg-white p-6 my-4 bg-gray-200">
+            <h2 className="text-3xl font-semibold text-gray-800 mb-4">Query Form</h2>
+            <form className="space-y-6" onSubmit={handleSubmit}>
+                {/* Feature 1 */}
+                <div className="flex flex-col md:flex-row items-center md:space-x-4">
+                    <label
+                        htmlFor="feature1"
+                        className="w-full md:w-1/3 text-sm font-medium text-gray-700 md:text-right"
+                    >
+                        Feature 1:
+                    </label>
+                    {/* <input
+                        type='text'
+                        id='feature1'
+                        value={feature1}
+                        className='w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 px-4 text-gray-700 focus:ring focus:ring-purple-100 focus:outline-none'
+                        onChange={(e) => setFeature1(e.target.value)}
+                    /> */}
+                    <select
+                        id="feature1"
                         value={feature1}
                         onChange={(e) => setFeature1(e.target.value)}
-                    />
+                        className="w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 px-4 text-gray-700 focus:ring focus:ring-purple-100 focus:outline-none"
+                    >
+                        <option value="">Select an option</option>
+                        {TEST_FEATURE_LIST.map((option, index) => (
+                            <option key={index} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-                <div>
-                    <label>Feature 2:</label>
-                    <input
-                        type="text"
+
+                {/* Feature 2 */}
+                <div className="flex flex-col md:flex-row items-center md:space-x-4">
+                    <label
+                        htmlFor="feature2"
+                        className="w-full md:w-1/3 text-sm font-medium text-gray-700 md:text-right"
+                    >
+                        Feature 2:
+                    </label>
+                    {/* <input
+                        type='text'
+                        id='feature2'
                         value={feature2}
+                        className='w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 px-4 text-gray-700 focus:ring focus:ring-purple-100 focus:outline-none'
                         onChange={(e) => setFeature2(e.target.value)}
+                    /> */}
+
+                    <MultiSelectDropdown
+                        formFieldName="feature2"
+                        options={TEST_FEATURE_LIST}
+                        onChange={(selected) => setFeature2(selected)}
+                        prompt="Select one or more features"
                     />
                 </div>
-                <div>
-                    <label>Minimum Correlation:</label>
+
+                {/* Minimum Correlation */}
+                <div className="flex flex-col md:flex-row items-center md:space-x-4">
+                    <label
+                        htmlFor="minCorrelation"
+                        className="w-full md:w-1/3 text-sm font-medium text-gray-700 md:text-right"
+                    >
+                        Minimum Correlation:
+                    </label>
                     <input
                         type="number"
+                        id="minCorrelation"
                         value={minCorrelation}
+                        className="w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 px-4 text-gray-700 focus:ring focus:ring-purple-100 focus:outline-none"
                         onChange={(e) => setMinCorrelation(e.target.value)}
                     />
                 </div>
-                <div>
-                    <label>Maximum P-Value:</label>
+
+                {/* Maximum P-Value */}
+                <div className="flex flex-col md:flex-row items-center md:space-x-4">
+                    <label
+                        htmlFor="maxPValue"
+                        className="w-full md:w-1/3 text-sm font-medium text-gray-700 md:text-right"
+                    >
+                        Maximum P-Value:
+                    </label>
                     <input
                         type="number"
                         step="any"
+                        id="maxPValue"
                         value={maxPValue}
+                        className="w-full md:w-1/2 bg-gray-100 border border-gray-300 rounded-lg py-2 px-4 text-gray-700 focus:ring focus:ring-purple-100 focus:outline-none"
                         onChange={(e) => setMaxPValue(e.target.value)}
                     />
                 </div>
-                <button type="submit">Proceed</button>
+
+                {/* Submit Button */}
+                <div className="flex justify-center">
+                    <button
+                        type="submit"
+                        className="bg-purple-500 text-white font-medium py-2 px-6 rounded-lg hover:bg-purple-600 focus:ring focus:ring-purple-100 focus:outline-none"
+                    >
+                        Query Database
+                    </button>
+                </div>
             </form>
         </div>
     );
